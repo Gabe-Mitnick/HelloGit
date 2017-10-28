cce9d38f0bdc1eec8fd542ea095b1f1a5d58d8f9
David Fang <795449@seq.org>
2017-10-28 14:51:12 -0700

    Merge with Gabe-Mitnick/HelloGit Add println(we out)

--- HelloGit/src/com/brettle/hellogit/HelloGit.java
+++ HelloGit/src/com/brettle/hellogit/HelloGit.java
@@ -5,7 +5,8 @@ public class HelloGit {
 	public String getMessage() {
 		// TODO Auto-generated method stub
 		System.out.println("sup fam");
-		System.out.println("Add println(Add println(Add println(...)");		
+		System.out.println("Add println(Add println(Add println(...)");
+		System.out.println("we out");
 		return "Hello Git!";
 	}
 
